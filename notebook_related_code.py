a7604ffb41ff15c96d61cbfc883f2762a0daf8b0
Christos Mavrogiannis <Christos.Mavrogiannis@ottobockde.onmicrosoft.com>
2022-07-27 23:27:24 +0200

    Started data preparation section

--- notebook_related_code.py
+++ notebook_related_code.py
@@ -18,3 +18,25 @@ display(data.head(n=1))
 
 # Total number of records
 n_records = len(data.index)
+
+# Number of records where individual's income is more than $50,000
+n_greater_50k = len(data.loc[data['income'].eq(">50K")])
+
+# Number of records where individual's income is at most $50,000
+n_at_most_50k = len(data.loc[data['income'].eq("<=50K")])
+
+# Percentage of individuals whose income is more than $50,000
+greater_percent = n_greater_50k/n_records
+
+# Print the results
+print("Total number of records: {}".format(n_records))
+print("Individuals making more than $50,000: {}".format(n_greater_50k))
+print("Individuals making at most $50,000: {}".format(n_at_most_50k))
+print("Percentage of individuals making more than $50,000: {}%".format(greater_percent))
+
+# Split the data into features and target label
+income_raw = data['income']
+features_raw = data.drop('income', axis = 1)
+
+# Visualize skewed continuous features of original data
+vs.distribution(data)
