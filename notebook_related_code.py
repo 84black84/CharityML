ad94a18e32eff368ea307fb54d1363b30f8f1745
Christos Mavrogiannis <Christos.Mavrogiannis@ottobockde.onmicrosoft.com>
2022-07-28 15:18:22 +0200

    Almost finishing Question 1

--- notebook_related_code.py
+++ notebook_related_code.py
@@ -40,3 +40,63 @@ features_raw = data.drop('income', axis = 1)
 
 # Visualize skewed continuous features of original data
 vs.distribution(data)
+
+# Log-transform the skewed features
+skewed = ['capital-gain', 'capital-loss']
+features_log_transformed = pd.DataFrame(data = features_raw)
+features_log_transformed[skewed] = features_raw[skewed].apply(lambda x: np.log(x + 1))
+
+# Visualize the new log distributions
+vs.distribution(features_log_transformed, transformed = True)
+
+# Import sklearn.preprocessing.StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+# Initialize a scaler, then apply it to the features
+scaler = MinMaxScaler() # default=(0, 1)
+numerical = ['age', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+
+features_log_minmax_transform = pd.DataFrame(data = features_log_transformed)
+features_log_minmax_transform[numerical] = scaler.fit_transform(features_log_transformed[numerical])
+
+# Show an example of a record with scaling applied
+display(features_log_minmax_transform.head(n = 5))
+
+# One-hot encode the 'features_log_minmax_transform' data using pandas.get_dummies()
+features_final = pd.get_dummies(features_log_minmax_transform)
+
+# TODO: Encode the 'income_raw' data to numerical values
+income = income_raw.replace({'>50K' : 1, '<=50K' : 0})
+
+# Print the number of features after one-hot encoding
+encoded = list(features_final.columns)
+print("{} total features after one-hot encoding.".format(len(encoded)))
+
+# Uncomment the following line to see the encoded feature names
+print(encoded)
+
+# Import train_test_split
+from sklearn.model_selection import train_test_split
+
+# Split the 'features' and 'income' data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features_final, income, test_size = 0.2, random_state = 0)
+
+# Show the results of the split
+print("Training set has {} samples.".format(X_train.shape[0]))
+print("Testing set has {} samples.".format(X_test.shape[0]))
+
+TP = np.sum(income) # adding all the rows is enough since all the rows with '0' value is ignored 
+FP = income.count() - TP # all the rows minus the ones they have '0' value
+TN = 0
+FN = 0
+# Calculate accuracy, precision and recall
+accuracy = TP / n_records
+# [True Positives/(True Positives + False Negatives)]
+recall = TP / (TP + FN)
+# [True Positives/(True Positives + False Positives)]
+precision = TP / (TP + FP)
+# Calculate F-score using the formula above for beta = 0.5 and correct values for precision and recall.
+fscore = None
+
+# Print the results 
+print("Naive Predictor: [Accuracy score: {:.4f}, F-score: {:.4f}]".format(accuracy, fscore))
